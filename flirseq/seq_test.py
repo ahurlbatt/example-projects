10d8d516ea550cc6dbee230be0ba9f43372ab625
Andrew Hurlbatt <andrewhurlbatt@gmail.com>
2022-03-22 18:57:43 +0100

    Update seq_test.py to use new ir_imaging module

--- flirseq/seq_test.py
+++ flirseq/seq_test.py
@@ -4,7 +4,7 @@ def run_test():
     import pathlib
 
     from flirseq import seq_reader
-    from flirseq import infroptics
+    from flirseq import ir_imaging
 
     # Find out where we are
     my_dir = pathlib.Path(__file__).parent.resolve()
@@ -17,19 +17,29 @@ def run_test():
     relative_humidity = 0.5
 
     # Create a list of optical components between the camera and the object
-    my_optics = [
-        infroptics.Atmosphere(background_temperature, 0.1, relative_humidity),
-        infroptics.Window(background_temperature, 0.97, 0.01, 0.01),
-        infroptics.Vacuum(0.3),
-        infroptics.Mirror(background_temperature, 0.99),
-        infroptics.Vacuum(1),
+    object_system = [
+        ir_imaging.Atmosphere(temperature=background_temperature, length=0.1, relative_humidity=relative_humidity),
+        ir_imaging.Window(
+            temperature=background_temperature,
+            background_temperature=background_temperature,
+            transmittance=0.97,
+            reflectance=0.02,
+            axis_alignment=True,
+        ),
+        ir_imaging.Vacuum(0.3),
+        ir_imaging.Mirror(
+            reflectance=0.99, temperature=background_temperature, background_temperature=background_temperature,
+        ),
+        ir_imaging.Vacuum(1),
     ]
 
     # Define the estimated emittance of the object of interest
-    object_emittance = 0.9
+    target_object = ir_imaging.TargetObject(
+        emittance=0.9, background_temperature=background_temperature, diffuse_fraction=1.0,
+    )
 
     # Pass these arguments to the top-level function for direct conversion to Kelvin of the last frame in the file
-    K, s = seq_reader.seq_to_kelvin(file_name, my_optics, background_temperature, object_emittance, frames="last")
+    K, s = seq_reader.seq_to_kelvin(file_name, target_object, object_system, frames="last")
 
     # Extract the times of the frames that have been read
     frame_times = s.frame_times
@@ -37,8 +47,8 @@ def run_test():
     # Show the last frame as an image in degrees Centigrade
     imgplt = plt.imshow(K[0] - 273.15)
 
-    return s, frame_times, imgplt, my_optics
+    return s, frame_times, imgplt, object_system
 
 
 if __name__ == "__main__":
-    s, frame_times, imgplt, my_optics = run_test()
+    s, frame_times, imgplt, object_system = run_test()
